605e85468115ea99415e75d13c37c86f03769faa
Ignacio Rosales <ignrosales1997@gmail.com>
2023-12-28 22:28:40 -0300

    streams

---
+++ 01-dart-intro/14_streams.dart
@@ -0,0 +1,37 @@
+void main() {
+  
+  emitNumbers().listen( ( value ){
+    print('Stream value $value');
+  });
+  
+}
+
+Stream<int> emitNumbers(){
+  
+  return Stream.periodic( Duration(seconds: 1), ( value ) {
+    return value;
+  }).take(5);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
