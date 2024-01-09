e59e0074bba916c0d930ce421036a3c9aaee0ca9
Ignacio Rosales <ignrosales1997@gmail.com>
2024-01-08 21:44:08 -0300

    Progress indicators

--- widgets_app/lib/config/theme/menu/menu_items.dart
+++ widgets_app/lib/config/theme/menu/menu_items.dart
@@ -26,4 +26,11 @@ const appMenuItems = <MenuItem>[
     link: '/cards',
     icon: Icons.credit_card
   ),
+
+  MenuItem(
+    title: 'ProgressIndicators',
+    subTitle: 'General and controlled',
+    link: '/progress',
+    icon: Icons.refresh_rounded
+  ),
 ];
