18e2b62aa075f0ff1af666ab8e60816ce78beeec
Ignacio Rosales <ignrosales1997@gmail.com>
2024-01-11 00:59:05 -0300

    Theme provider!

--- widgets_app/lib/presentation/screens/theme_changer/theme_changer_screen.dart
+++ widgets_app/lib/presentation/screens/theme_changer/theme_changer_screen.dart
@@ -10,7 +10,7 @@ class ThemeChangerScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
 
-    final isDarkMode = ref.watch(isDarkModeProvider);
+    final isDarkMode = ref.watch(themeNotifierProvider).isDarkMode;
 
     return Scaffold(
       appBar: AppBar(
@@ -18,7 +18,7 @@ class ThemeChangerScreen extends ConsumerWidget {
         actions:[
           IconButton(
             onPressed: (){
-              ref.read(isDarkModeProvider.notifier).update((state) => !state);
+              ref.read(themeNotifierProvider.notifier).toggleDarkMode();
             }, 
             icon: Icon(isDarkMode ? Icons.light_mode_outlined : Icons.dark_mode_outlined))
         ],
@@ -35,7 +35,7 @@ class _ThemeChangerView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
 
     final List<Color> colors = ref.watch(colorListProvider);
-    final int selectedColor = ref.watch(selectedColorProvider);
+    final int selectedColor = ref.watch(themeNotifierProvider).selectedColor;
 
     return ListView.builder(
       itemCount: colors.length,
@@ -49,7 +49,7 @@ class _ThemeChangerView extends ConsumerWidget {
           value: index, 
           groupValue: selectedColor, 
           onChanged: (value){
-              ref.read(selectedColorProvider.notifier).state = index;
+              ref.read(themeNotifierProvider.notifier).changeColorIndex(index);
           }
         );
       }
