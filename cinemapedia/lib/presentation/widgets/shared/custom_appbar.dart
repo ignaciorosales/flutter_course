476ff05445b7b7364c525fa38370bc175fcaec37
Ignacio Rosales <ignrosales1997@gmail.com>
2024-01-17 17:30:24 -0300

    cinema app progress

--- cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
+++ cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,9 +1,9 @@
-import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
-import 'package:cinemapedia/presentation/screens/movies/providers/movies/movies_repository_provider.dart';
+import 'package:cinemapedia/presentation/screens/movies/providers/search/search_movies_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 
 class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
@@ -14,36 +14,44 @@ class CustomAppbar extends ConsumerWidget {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
 
-    return  SafeArea(
+    return SafeArea(
       bottom: false,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
+        padding: const EdgeInsets.symmetric( horizontal: 10 ),
         child: SizedBox(
           width: double.infinity,
           child: Row(
             children: [
-              Icon(Icons.movie_outlined, color: colors.primary),
-              const SizedBox(width: 5,),
-              Text('Cinemapedia', style: titleStyle),
+              Icon( Icons.movie_outlined, color: colors.primary ),
+              const SizedBox( width: 5 ),
+              Text('Cinemapedia', style: titleStyle ),
+      
               const Spacer(),
+      
               IconButton(onPressed: () {
 
-                final movieRepository = ref.read(movieRepositoryProvider);
-
+                final searchedMovies = ref.read( searchedMoviesProvider );
+                final searchQuery = ref.read(searchQueryProvider);
+                
                 showSearch<Movie?>(
-                  context: context,
+                  query: searchQuery,
+                  context: context, 
                   delegate: SearchMovieDelegate(
-                    searchMovies: movieRepository.searchMovies
+                    initialMovies: searchedMovies,
+                    searchMovies: ref.read( searchedMoviesProvider.notifier ).searchMoviesByQuery
                   )
-                ).then((movie){
-                  if( movie != null) context.push('/movie/${movie.id}');
+                ).then((movie) {
+                  if ( movie == null ) return;
+
+                  context.push('/movie/${ movie.id }');
                 });
 
               }, 
-              icon: const Icon(Icons.search))
+              icon: const Icon(Icons.search)
+              )
             ],
-          )
-        )
+          ),
+        ),
       )
     );
   }
