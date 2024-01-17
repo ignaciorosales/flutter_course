a555fd4af9cee284437eb68d00ed535730b8f7b0
Ignacio Rosales <ignrosales1997@gmail.com>
2024-01-17 17:30:18 -0300

    updating delegate and datasource

--- cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
+++ cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
@@ -1,130 +1,218 @@
-import 'package:animate_do/animate_do.dart';
-import 'package:cinemapedia/config/helpers/human_formats.dart';
-import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+import 'package:cinemapedia/config/helpers/human_formats.dart';
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function( String query );
 
 class SearchMovieDelegate extends SearchDelegate<Movie?>{
-  SearchMovieDelegate({
-    required this.searchMovies
-  });
+
 
   final SearchMoviesCallback searchMovies;
+  List<Movie> initialMovies;
+  
+  StreamController<List<Movie>> debouncedMovies = StreamController.broadcast();
+  StreamController<bool> isLoadingStream = StreamController.broadcast();
 
-  @override
-  String get searchFieldLabel => 'Search movie';
 
-  @override
-  List<Widget>? buildActions(BuildContext context) {
+  Timer? _debounceTimer;
 
-    return [
-        FadeIn(
-          animate: query.isNotEmpty,
-          child: IconButton(
-            onPressed: () => query = '', 
-            icon: const Icon(Icons.clear),
-          ),
-        )
-    ];
+  SearchMovieDelegate({
+    required this.searchMovies,
+    required this.initialMovies,
+  }):super(
+    searchFieldLabel: 'Buscar películas',
+    // textInputAction: TextInputAction.done
+  );
+
+  void clearStreams() {
+    debouncedMovies.close();
   }
 
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () => close(context, null), 
-      icon: const Icon(Icons.arrow_back),
-    );
+  void _onQueryChanged( String query ) {
+    isLoadingStream.add(true);
+
+    if ( _debounceTimer?.isActive ?? false ) _debounceTimer!.cancel();
+
+    _debounceTimer = Timer(const Duration( milliseconds: 500 ), () async {
+      // if ( query.isEmpty ) {
+      //   debouncedMovies.add([]);
+      //   return;
+      // }
+
+      final movies = await searchMovies( query );
+      initialMovies = movies;
+      debouncedMovies.add(movies);
+      isLoadingStream.add(false);
+
+    });
+
   }
 
-  @override
-  Widget buildResults(BuildContext context) {
-    return const Text('BuildResults');
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return FutureBuilder(
-      future: searchMovies(query),
-      builder:(context, snapshot) {
+  Widget buildResultsAndSuggestions() {
+    return StreamBuilder(
+      initialData: initialMovies,
+      stream: debouncedMovies.stream,
+      builder: (context, snapshot) {
+        
         final movies = snapshot.data ?? [];
 
         return ListView.builder(
           itemCount: movies.length,
-          itemBuilder:(context, index) =>_MovieSearchItem(
+          itemBuilder: (context, index) => _MovieItem(
             movie: movies[index],
-            onMovieSelected: close,
-          )
+            onMovieSelected: (context, movie) {
+              clearStreams();
+              close(context, movie);
+            },
+          ),
         );
       },
     );
   }
 
 
+  // @override
+  // String get searchFieldLabel => 'Buscar película';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+
+    return [
+
+      StreamBuilder(
+        initialData: false,
+        stream: isLoadingStream.stream,
+        builder: (context, snapshot) {
+            if ( snapshot.data ?? false ) {
+              return SpinPerfect(
+                  duration: const Duration(seconds: 20),
+                  spins: 10,
+                  infinite: true,
+                  child: IconButton(
+                    onPressed: () => query = '', 
+                    icon: const Icon( Icons.refresh_rounded )
+                  ),
+                );
+            }
+
+             return FadeIn(
+                animate: query.isNotEmpty,
+                child: IconButton(
+                  onPressed: () => query = '', 
+                  icon: const Icon( Icons.clear )
+                ),
+              );
+
+        },
+      ),
+      
+       
+        
+
+
+
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+          clearStreams();
+          close(context, null);
+        }, 
+        icon: const Icon( Icons.arrow_back_ios_new_rounded)
+      );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return buildResultsAndSuggestions();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+
+    _onQueryChanged(query);
+    return buildResultsAndSuggestions();
+
+  }
+
 }
 
-
-class _MovieSearchItem extends StatelessWidget {
-  const _MovieSearchItem({
-    required this.movie, 
-    required this.onMovieSelected
-  });
+class _MovieItem extends StatelessWidget {
 
   final Movie movie;
   final Function onMovieSelected;
 
+  const _MovieItem({
+    required this.movie,
+    required this.onMovieSelected
+  });
+
   @override
   Widget build(BuildContext context) {
 
-    final textStyle = Theme.of(context).textTheme;
+    final textStyles = Theme.of(context).textTheme;
     final size = MediaQuery.of(context).size;
 
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         onMovieSelected(context, movie);
       },
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         child: Row(
           children: [
+    
+            // Image
             SizedBox(
-              width: size.width*0.2,
+              width: size.width * 0.2,
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
-                child: Image.network(
+                child: Image.network( 
                   movie.posterPath,
-                  loadingBuilder: (context, child, loadingProgress) => FadeIn(child : child),
-                )
-              )
+                  loadingBuilder: (context, child, loadingProgress) => FadeIn(child: child),
+                ),
+              ),
             ),
-      
-            const SizedBox(width: 10,),
-      
+    
+            const SizedBox(width: 10),
+            
+            // Description
             SizedBox(
-              width: size.width*0.7,
+              width: size.width * 0.7,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(movie.title, style: textStyle.titleMedium),
-                  (movie.overview.length>100)
-                  ? Text('${movie.overview.substring(0,100)}...')
-                  : Text(movie.overview),
-      
+                  Text( movie.title, style: textStyles.titleMedium ),
+    
+                  ( movie.overview.length > 100 )
+                    ? Text( '${movie.overview.substring(0,100)}...' )
+                    : Text( movie.overview ),
+    
                   Row(
                     children: [
-                      Icon(Icons.star_half_rounded, color: Colors.yellow.shade800),
-                      const SizedBox(width: 5,),
-                      Text(
-                        HumanFormats.number(movie.voteAverage, 1).toString(),
-                        style: textStyle.bodyMedium!.copyWith(color: Colors.yellow.shade900)
-                      )
+                      Icon( Icons.star_half_rounded, color: Colors.yellow.shade800 ),
+                      const SizedBox(width: 5),
+                      Text( 
+                        HumanFormats.number(movie.voteAverage, 1),
+                        style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900 ),
+                      ),
                     ],
                   )
+    
+                  
                 ],
-              )
-            )
-          ]
-        )
+              ),
+            ),
+    
+          ],
+        ),
       ),
     );
   }
