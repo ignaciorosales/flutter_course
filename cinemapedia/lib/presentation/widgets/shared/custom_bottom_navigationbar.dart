9c82ca4ae611f041d814874e3648a2321fa6391e
Ignacio Rosales <ignrosales1997@gmail.com>
2024-01-19 00:02:00 -0300

    custom bottom navigation

--- cinemapedia/lib/presentation/widgets/shared/custom_bottom_navigationbar.dart
+++ cinemapedia/lib/presentation/widgets/shared/custom_bottom_navigationbar.dart
@@ -1,62 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+
 class CustomBottomNavigationBar extends StatelessWidget {
-  const CustomBottomNavigationBar({super.key});
 
-  int getCurrentIndex(BuildContext context) {
-    final String location = GoRouterState.of(context).matchedLocation;
+  final int currentIndex;
 
-    switch(location)
-    {
-      case '/':
-        return 0;
-      case '/categories':
-        return 1;
-      case '/favorites':
-        return 2;
-      default:
-        return 0;
-    }
-  }
+  const CustomBottomNavigationBar({
+    super.key, 
+    required this.currentIndex
+  });
 
-  void onItemTapped(BuildContext context, int index){
-    switch(index){
+  void onItemTapped( BuildContext context, int index ) {
+    // context.go('');
+    switch(index) {
       case 0:
-        context.go('/');
+        context.go('/home/0');
         break;
+      
       case 1:
-        context.go('/');
+        context.go('/home/1');
         break;
+
       case 2:
-        context.go('/favorites');
+        context.go('/home/2');
         break;
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (value) => onItemTapped(context, value),
       elevation: 0,
-      currentIndex: getCurrentIndex(context),
-      onTap: (index) => onItemTapped(context, index),
       items: const [
-
         BottomNavigationBarItem(
-          icon: Icon(Icons.home_max),
-          label: 'Home'
-          ),
-
-        BottomNavigationBarItem(
-          icon: Icon(Icons.label_outline),
-          label: 'Categories'
+          icon: Icon( Icons.home_max ),
+          label: 'Inicio'
         ),
-
         BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_outline),
-          label: 'Favorite'
+          icon: Icon( Icons.label_outline ),
+          label: 'Categorías'
         ),
-        
+        BottomNavigationBarItem(
+          icon: Icon( Icons.favorite_outline ),
+          label: 'Favoritos'
+        ),
       ]
     );
   }
